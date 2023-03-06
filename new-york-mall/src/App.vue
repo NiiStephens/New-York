1f12d475b6b7f0600bbb5520d24df160c14b0140
Nii <bnstephens001@gmail.com>
2023-03-06 08:19:24 +0000

    Added Heading to the Project and state the stack using for the project. Project is half-baked

--- new-york-mall/src/App.vue
+++ new-york-mall/src/App.vue
@@ -4,29 +4,34 @@
       <h1>New York Construction Mall Project</h1>
 
       <h6> This app is powererd by Vue 3 |Axios| Ruby on Rails| Postgresql|</h6>
+      
     </div>
-
+    
   </div>
 </template>
 
 <script>
 
-
 export default {
   name: 'App',
   components: {
-  
+
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+  line-height: 1.6;
+  background: whitesmoke;
+}
+
+.container {
+  max-width: 1100px;
+  margin: auto;
+  overflow: auto;
+  padding: 0.2rem;
+
 }
 </style>
